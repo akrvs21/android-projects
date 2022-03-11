c9219f239edd7c1d1f6ceeb96100b5b85b525ae2
raider2107 <3108987378@qq.com>
2022-03-11 16:44:26 +0500

    add login module

---
+++ Yrocery/app/src/main/java/com/example/yrocery/Login.java
@@ -0,0 +1,71 @@
+package com.example.yrocery;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Login extends AppCompatActivity {
+    EditText editPhone, editPassword;
+    Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editPhone = findViewById(R.id.editPhone);
+        editPassword = findViewById(R.id.editPassword);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        // Init Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://yrocery-default-rtdb.asia-southeast1.firebasedatabase.app");
+        DatabaseReference table_user = database.getReference("User");
+        Log.d("mydb", String.valueOf(table_user));
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressDialog progressDialog = new ProgressDialog(Login.this);
+                progressDialog.setMessage("Please wait....");
+                progressDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        // Check if user not exist in database
+                        if(snapshot.child(editPhone.getText().toString()).exists()) {
+                            progressDialog.dismiss();
+                            // Get User information
+                            User user = snapshot.child(editPhone.getText().toString()).getValue(User.class);
+                            assert user != null;
+                            if(user.getPassword().equals(editPassword.getText().toString())) {
+                                Toast.makeText(Login.this, "Login successfully", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(Login.this, "Login failed", Toast.LENGTH_SHORT).show();
+                            }
+                         } else {
+                            Toast.makeText(Login.this, "The user doesn't exist", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ Yrocery/app/src/main/java/com/example/yrocery/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.yrocery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        // To sign up
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // To sign in
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(MainActivity.this, Login.class);
+                startActivity(loginIntent);
+            }
+        });
+    }
+}
+
+
